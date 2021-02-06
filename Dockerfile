76fc1830e23998b374d48cfe21f82d2752c2f8fb
Luke Blaney <git@lukeblaney.co.uk>
2021-02-06 19:50:33 +0000

    Add /_info endpoint

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM php:8-apache
 
 WORKDIR /var/www/html/
 
-
+# Use the default production configuration
+RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+RUN a2enmod rewrite
 
 COPY src/. .
 
---
+++ src/_info.php
@@ -0,0 +1,23 @@
+<?php
+	header("Content-Type: application/json");
+	$output = [
+		"system" => "lucos_media_metadata_manager",
+		"ci" => [
+			"circle" => "gh/lucas42/lucos_media_metadata_manager",
+		],
+		"checks" => [
+			"metadata-api" => [
+				"techDetail" => "Can connect to lucos media metadata API",
+			]
+		],
+	];
+
+	// TODO: don't hardcode this URL, use same fetch logic as elsewhere.
+	$tracks = file_get_contents("https://media-api.l42.eu/tracks");
+	if ($tracks === false) {
+		$output["checks"]["metadata-api"]["debug"] = error_get_last()["message"];
+		$output["checks"]["metadata-api"]["ok"] = false;
+	} else {
+		$output["checks"]["metadata-api"]["ok"] = true;
+	}
+	echo json_encode($output);
