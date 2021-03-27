01707794dee1d7023164bf8bb8e0f81552440c8f
Luke Blaney <git@lukeblaney.co.uk>
2021-03-27 04:16:56 +0000

    do single call to API for updating all tags in a given track

--- src/controllers/updatetrack.php
+++ src/controllers/updatetrack.php
@@ -6,21 +6,18 @@ require_once("../formfields.php");
  * Sets the value to of each field to the value in $postdata for that key UNLESS there exists a key for that field suffixed with "_null", in which case the field is deleted
  **/
 function updateTrack($trackid, $postdata) {
+	$tags = array();
 	foreach (getFormKeys() as $key) {
-
-		// If a tag is marked as null, DELETE it from the API
-		// Otherwise, update the value using PUT
-		$method = array_key_exists("${key}_null", $postdata) ? "DELETE" : "PUT";
-		$val = array_key_exists($key, $postdata) ? $postdata[$key] : null;
-		$tagurl = "https://media-api.l42.eu/tags/${trackid}/${key}";
-		$context = stream_context_create([
-			"http" => [
-				"method" => $method,
-				"header" => "Content-Type: text/plain",
-				"content" => $val,
-			],
-		]);
-		file_get_contents($tagurl, false, $context);
+		$tags[$key] = array_key_exists("${key}_null", $postdata) ? null : $postdata[$key];
 	}
+	$trackurl = "https://media-api.l42.eu/tracks/${trackid}";
+	$context = stream_context_create([
+		"http" => [
+			"method" => "PATCH",
+			"header" => "Content-Type: application/json",
+			"content" => json_encode(["tags" => $tags]),
+		],
+	]);
+	file_get_contents($trackurl, false, $context);
 	header("Location: /tracks/${trackid}?saved=true", true, 303);
 }
