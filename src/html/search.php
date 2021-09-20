374d4240c1c0fd1df618eb0c2784e32c6a9e300a
Luke Blaney <git@lukeblaney.co.uk>
2021-09-20 01:23:44 +0100

    prevent warning regarding unset page key

--- src/html/search.php
+++ src/html/search.php
@@ -3,6 +3,7 @@
 require("../controllers/searchtracks.php");
 
 if (!empty($_GET['q'])) {
+	if (empty($_GET['page'])) $_GET['page'] = null;
 	searchTracks($_GET['q'], $_GET['page']);
 } else {
 	require("../views/home.php"); // TODO: replace with page that just has search box
