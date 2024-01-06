487918c44178e518a1791f8a01f7adf5ed8912d4
Luke Blaney <git@lukeblaney.co.uk>
2024-01-06 20:24:52 +0000

    add Swahili to the language options

--- src/formfields.php
+++ src/formfields.php
@@ -98,6 +98,7 @@ function getTagFields() {
 					"mjy" => "Mohican",
 					"cmn" => "Mandarin",
 					"ja" => "Japanese",
+					"sw" => "Swahili",
 					"art-x-von" => "Hopelandic", // Fictional language created and used by Sigur Rós, also known as Vonlenska
 					"art-x-simlish" => "Simlish", // Fictional language used in The Sims in-game universe
 					"mul" => "Multiple Languages",
