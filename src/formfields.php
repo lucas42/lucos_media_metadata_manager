dd04a297fd18594e9648e71b805982fe1d32ebcf
Luke Blaney <git@lukeblaney.co.uk>
2024-01-06 21:08:35 +0000

    add Maninka to the language options NB: don't fully understand the ISO 639 codes for Maninka - have chosen emk, which is technically "Eastern Maninkakan".

--- src/formfields.php
+++ src/formfields.php
@@ -101,6 +101,7 @@ function getTagFields() {
 					"sw" => "Swahili",
 					"zu" => "Zulu",
 					"mn" => "Mongolian",
+					"emk" => "Maninka",
 					"art-x-von" => "Hopelandic", // Fictional language created and used by Sigur Rós, also known as Vonlenska
 					"art-x-simlish" => "Simlish", // Fictional language used in The Sims in-game universe
 					"mul" => "Multiple Languages",
